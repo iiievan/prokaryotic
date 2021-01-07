7f595402d5380ab7ef7788d23bfd86045eefcb43
i.shopov <mbo.kastnev@gmail.com>
2021-01-07 17:31:50 +0300

    Добавил класс камера и возможность плавать во вращающихся кубах в доволь.

--- include.h
+++ include.h
@@ -16,5 +16,8 @@
 #define VSHADER_PATH  "shaders/vertex.shader"
 #define FSHADER_PATH  "shaders/fragment.shader"
 
+void debInput(GLFWwindow* window, int key, int scancode, int action, int mods);
+void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window, int key, int scancode, int action, int mods);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include "include.h"
 #include "Shader.h"
+#include "Camera.h"
 
 Shader *ourShader;
 
 float alpha {0.2};
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float dT = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 int main()
 {
@@ -88,7 +99,11 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetKeyCallback(window, processInput);	
+	glfwSetKeyCallback(window, debInput);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // Отключаем курсор в окне чтобы можно было плавать в любом направлении.
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -166,7 +181,14 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Обработка клавиш и мыши происходит автоматом в каллбэке processInput*/
+		/* Обработка клавиш c антидребезгом происходит в  debInput*/
+		// per-frame time logic
+		// --------------------
+		float currentFrame = glfwGetTime();
+		dT = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -182,12 +204,13 @@ int main()
 
 		glBindVertexArray(VAO);
 
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f));
-		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		ourShader->setMat4("view", view);
+		// pass projection matrix to shader (note that in this case it could change every frame)
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		ourShader->setMat4("projection", projection);
+
+		// camera/view transformation
+		glm::mat4 view = camera.GetViewMatrix();
+		ourShader->setMat4("view", view);		
 
 		for (unsigned int i = 0; i < 10; i++)
 		{
@@ -215,7 +238,7 @@ int main()
 	return 0;
 }
 
-void processInput(GLFWwindow* window, int key, int scancode, int action, int mods)
+void debInput(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	static GLboolean wireframe{ GL_FALSE }; // для отображения режима каркаса.
 
@@ -241,7 +264,45 @@ void processInput(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 }
 
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, dT);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, dT);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, dT);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, dT);
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
 }
