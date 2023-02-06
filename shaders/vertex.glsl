c3c5946577351b09b79a69391ca90426f8a297c1
iiievan <iiievan@yandex.ru>
2023-02-06 13:28:54 +0300

    1.Adjust the vertex shader so that the triangle is upside down: solution. 2.Specify a horizontal offset via a uniform and move the triangle to the right side of the screen in the vertex shader using this offset value: solution.
    For upside down: just tape -a_Pos.y in vertex shader.

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -2,10 +2,12 @@
 layout (location = 0) in vec3 a_Pos;
 layout (location = 1) in vec3 a_Color;
 
+uniform float u_Offset;
+
 out vec3 our_Color;
 
 void main()
 {
-    gl_Position = vec4(a_Pos.x, a_Pos.y, a_Pos.z, 1.0);
+    gl_Position = vec4(a_Pos.x + u_Offset, a_Pos.y, a_Pos.z, 1.0);
     our_Color = a_Color;
 }
