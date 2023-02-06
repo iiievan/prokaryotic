bafe20781d0e001c0131837e7cf5b34b946f32ec
iiievan <iiievan@yandex.ru>
2023-02-06 13:30:24 +0300

    Upside down.

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -8,6 +8,6 @@ out vec3 our_Color;
 
 void main()
 {
-    gl_Position = vec4(a_Pos.x + u_Offset, a_Pos.y, a_Pos.z, 1.0);
+    gl_Position = vec4(a_Pos.x + u_Offset, -a_Pos.y, a_Pos.z, 1.0);
     our_Color = a_Color;
 }
