56a9a43a8a918c33ddbaa34007c511e0dac8f89e
mbo_kastnev <mbo.kastnev@gmail.com>
2023-07-28 01:35:36 +0300

    Texture class redesign;

--- prokaryotic/src/model/Texture.cpp
+++ prokaryotic/src/model/Texture.cpp
@@ -1,9 +1,58 @@
-#include "texture.h"
+#include "Texture.h"
 
 #include <assert.h>
+#include <string>
 
 namespace PROKARYOTIC
 {
+    Texture::Texture(std::string  fname,             // file name of texture
+                          GLenum  texture_type,      // type of texture: [ GL_TEXTURE_1D, GL_TEXTURE_2D ,GL_TEXTURE_3D, GL_TEXTURE_CUBE_MAP ]
+                 texture_param_t  params)
+    {
+        m_texture_type = texture_type;
+        glGenTextures(1, &m_ID);
+
+        Bind();
+
+        switch (m_texture_type)
+        {
+            case GL_TEXTURE_1D:
+                glTexImage1D(m_texture_type, 0, params.internal_format, params.width, 0, 
+                             params.format, params.pixel_datatype, params.p_image_data);
+                glTexParameteri(m_texture_type, GL_TEXTURE_MIN_FILTER, params.filter_max);
+                glTexParameteri(m_texture_type, GL_TEXTURE_MAG_FILTER, params.filter_min);
+                glTexParameteri(m_texture_type, GL_TEXTURE_WRAP_S, params.wrap_S);
+                break;
+            case GL_TEXTURE_2D:
+                glTexImage2D(m_texture_type, 0, params.internal_format, 
+                             params.width, params.height, 0, 
+                             params.format, params.pixel_datatype, params.p_image_data);
+                glTexParameteri(m_texture_type, GL_TEXTURE_MIN_FILTER, params.filter_max);
+                glTexParameteri(m_texture_type, GL_TEXTURE_MAG_FILTER, params.filter_min);
+                glTexParameteri(m_texture_type, GL_TEXTURE_WRAP_S, params.wrap_S);
+                glTexParameteri(m_texture_type, GL_TEXTURE_WRAP_T, params.wrap_T);
+                break;
+            case GL_TEXTURE_3D:
+                glTexImage3D(m_texture_type, 0, params.internal_format, 
+                            params.width, params.height, params.depth, 0, 
+                            params.format, params.pixel_datatype, params.p_image_data);
+                glTexParameteri(m_texture_type, GL_TEXTURE_MIN_FILTER, params.filter_max);
+                glTexParameteri(m_texture_type, GL_TEXTURE_MAG_FILTER, params.filter_min);
+                glTexParameteri(m_texture_type, GL_TEXTURE_WRAP_S, params.wrap_S);
+                glTexParameteri(m_texture_type, GL_TEXTURE_WRAP_T, params.wrap_T);
+                glTexParameteri(m_texture_type, GL_TEXTURE_WRAP_R, params.wrap_R);
+                break;
+            case GL_TEXTURE_CUBE_MAP:
+                break;
+            defalt:
+                break;
+        }
+
+        if (params.mipmapping)
+            glGenerateMipmap(m_texture_type);
+
+        Unbind();
+    }
 
     void Texture::Generate(unsigned int width, GLenum internalFormat, GLenum format, GLenum type, void* data)
     {
@@ -16,7 +65,7 @@ namespace PROKARYOTIC
         m_Format = format;
         m_Type = type;
 
-        assert(m_Target == GL_TEXTURE_1D);
+        //assert(m_Target == GL_TEXTURE_1D);
         Bind();
 
         glTexImage1D(m_Target, 0, internalFormat, width, 0, format, type, data);
@@ -41,7 +90,7 @@ namespace PROKARYOTIC
         m_Format = format;
         m_Type = type;
 
-        assert(m_Target == GL_TEXTURE_2D);
+        //assert(m_Target == GL_TEXTURE_2D);
         Bind();
 
         glTexImage2D(m_Target, 0, internalFormat, width, height, 0, format, type, data);
@@ -67,7 +116,7 @@ namespace PROKARYOTIC
         m_Format = format;
         m_Type = type;
 
-        assert(m_Target == GL_TEXTURE_3D);
+        //assert(m_Target == GL_TEXTURE_3D);
         Bind();
 
         glTexImage3D(m_Target, 0, internalFormat, width, height, depth, 0, format, type, data);
@@ -105,17 +154,18 @@ namespace PROKARYOTIC
         }
     }
 
-    void Texture::Bind(int unit)
+    void Texture::Bind(int i)
     {
-        if (unit >= 0)
-            glActiveTexture(GL_TEXTURE0 + unit);
+        // i - number of array of texture units ~ 8 arrays for simultaneously texture drawing;
+        if (i >= 0)
+            glActiveTexture(GL_TEXTURE0 + i);   // 0 and GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS.
 
-        glBindTexture(m_Target, m_ID);
+        glBindTexture(m_texture_type, m_ID);
     }
 
     void Texture::Unbind()
     {
-        glBindTexture(m_Target, 0);
+        glBindTexture(m_texture_type, 0);
     }
 
     void Texture::set_GL_Wrap_mode(GLenum wrapMode, bool bind)
